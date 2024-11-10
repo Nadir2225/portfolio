3efb59be341af41210bf9b3f6d250e004b6164e3
Nadir2225 <nadirna2003@gmail.com>
2024-11-10 21:44:25 +0100

    developped the about page

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import img from '../assets/about-ill.png'
 
 const About = (props) => {
   return (
     <div ref={props.about} style={{width: '100%', minHeight: '100vh'}}>
         <div className='ms-5 titles' style={{ display: 'flex', gap: '10px'}}>About<div></div></div>
-        <div style={{width: '500px'}}>
-          Hello! I'm Nadir El Ouadghiri, a versatile software developer skilled in full-stack web development, Android application development, and game development. With a passion for crafting engaging and intuitive user experiences, I enjoy taking on challenges across different domains—whether it's building seamless web applications, designing native mobile apps, or creating interactive 2D games. My expertise spans both front-end and back-end technologies, as well as game development engines, and I constantly seek new ways to improve my skills and explore innovative solutions. Feel free to explore my work or reach out to discuss potential collaborations!
+        <div className='about-container'>
+          <div className='about-paragraph'>
+            Hello! I'm Nadir El Ouadghiri, a software developer from Rabat, Morocco, with skills in full-stack web development, Android app development, and game development. I'm passionate about creating engaging and user-friendly experiences, whether through seamless web applications, native mobile apps, or interactive games. My expertise covers both front-end and back-end technologies, and I'm always exploring new ways to innovate and improve. Let's connect and bring ideas to life!
+          </div>
+          <div className='about-ill'>
+            <img src={img} />
+          </div>
         </div>
     </div>
   )
--- src/sections/skills.jsx
+++ src/sections/skills.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Skills = (props) => {
   return (
     <div ref={props.skills} style={{width: '100%', minHeight: '100vh'}}>
-        skills
+        <div className='ms-5 titles' style={{ display: 'flex', gap: '10px'}}>Skills<div></div></div>
     </div>
   )
 }
